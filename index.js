8f7f9e3c23d56bbc88ebd3a6de2c2bc910517500
cynthia chebet <chebetcynthia8@gmail.com>
2021-08-25 12:44:12 +0300

    added subtraction function

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ function add(a, b) {
   return a + b;
 }
 
+function sub(a,b){
+  return b-a;
+}
+
 initial();
 
 const sum = add(1, 2);
+const subtraction = sub(5,8);
 console.log('sum ...', sum);
+console.log('subtract ...', subtraction);
