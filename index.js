53c4e31a8d179fd08c262bb48a71c1f0ee04b7a0
Felix Maiko <maikofelix89@gmail.com>
2021-08-25 12:54:33 +0300

    Added concat text function (#3)

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ function add(a, b) {
 function sub(a,b){
   return b-a;
 }
+function concatText(a,b){
+   return `${a} ${b}`;
+}
 
 initial();
 
 const sum = add(1, 2);
 const subtraction = sub(5,8);
+const concatedText = concatText("Hello","World");
 console.log('sum ...', sum);
 console.log('subtract ...', subtraction);
+console.log('concatedText', concatedText);
