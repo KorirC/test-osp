fc09101ba28fd44cef6958c566b794ed6496988f
cynthia chebet <chebetcynthia8@gmail.com>
2021-08-25 12:55:02 +0300

    modulus function

--- index.js
+++ index.js
@@ -12,11 +12,18 @@ function concatText(a,b){
    return `${a} ${b}`;
 }
 
+function modulus(a,b){
+  return b%a;
+}
+
 initial();
 
 const sum = add(1, 2);
 const subtraction = sub(5,8);
 const concatedText = concatText("Hello","World");
+const mod = modulus(3,8);
 console.log('sum ...', sum);
 console.log('subtract ...', subtraction);
 console.log('concatedText', concatedText);
+console.log('modulus ...',mod);
+
